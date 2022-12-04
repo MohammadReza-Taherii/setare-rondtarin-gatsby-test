f896ae21ae082277551bb855e5a6799180f0f46d
m.taheri <moreta94@ymail.com>
2022-12-04 10:06:08 +0330

    some changes

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return <main>About Page Gatsby</main>;
+};
+
+export default AboutPage;
+
+export const Head = () => <title>About Page</title>;
